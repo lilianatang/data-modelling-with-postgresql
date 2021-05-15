cb07d9b5caaeca46c8832dc6deecdf4cfff56a72
root <root@EDML039.chandos.com>
2021-05-15 13:43:28 -0600

    fixed bugs in sql_queries.py file so running python create_queries.py will create tables in postgresql database

--- create_tables.py
+++ create_tables.py
@@ -1,15 +1,11 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-def load_data():
-    
-
 def create_database():
     """
     - Creates and connects to the sparkifydb
     - Returns the connection and cursor to sparkifydb
     """
-    
     # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
     conn.set_session(autocommit=True)
--- sql_queries.py
+++ sql_queries.py
@@ -1,82 +1,95 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP table songplay"
-user_table_drop = "DROP table users"
-song_table_drop = "DROP table songs"
-artist_table_drop = "DROP table artists"
-time_table_drop = "DROP table time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = (
     """
-    CREATE TABLE songplay (
-        songplay_id SERIAL PRIMARY KEY,
-        session_id INTEGER NOT NULL,
-        location VARCHAR(255) NOT NULL,
-        user_agent VARCHAR(255) NOT NULL,
-        FOREIGN KEY (start_time)
-            REFERENCES time (start_time)
-            ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (user_id)
-            REFERENCES users (user_id)
-            ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (artist_id)
-            REFERENCES artists (artist_id)
-            ON UPDATE CASCADE ON DELETE CASCADE
-    )    
+    CREATE TABLE IF NOT EXISTS songplay (
+        songplay_id int GENERATED ALWAYS AS IDENTITY,
+        session_id int NOT NULL,
+        location varchar NOT NULL,
+        user_agent varchar NOT NULL,
+        start_time timestamp,
+        user_id int,
+        artist_id varchar,
+        PRIMARY KEY(songplay_id),
+        CONSTRAINT fk_time
+            FOREIGN KEY (start_time)
+                REFERENCES time (start_time)
+                ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT fk_user
+            FOREIGN KEY (user_id)
+                REFERENCES users (user_id)
+                ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT fk_artist
+            FOREIGN KEY (artist_id)
+                REFERENCES artists (artist_id)
+                ON UPDATE CASCADE ON DELETE CASCADE
+    );    
     """
 )
 
 user_table_create = (
     """
-    CREATE TABLE users (
-        user_id SERIAL PRIMARY KEY,
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
-        gender VARCHAR(1) NOT NULL,
-        level VARCHAR(255) NOT NULL
-    )    
+    CREATE TABLE IF NOT EXISTS users (
+        user_id int GENERATED ALWAYS AS IDENTITY,
+        first_name varchar NOT NULL,
+        last_name varchar NOT NULL,
+        gender varchar NOT NULL,
+        level varchar NOT NULL,
+        PRIMARY KEY(user_id)
+    );    
     """
 )
 
 song_table_create = (
     """
-    CREATE TABLE songs (
-        song_id VARCHAR(255) PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        year INTEGER NOT NULL,
-        duration NUMERIC NOT NULL,
-        FOREIGN KEY (artist_id)
-            REFERENCES time artists (artist_id)
-            ON UPDATE CASCADE ON DELETE CASCADE
-    )    
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id varchar,
+        title varchar NOT NULL,
+        year int NOT NULL,
+        duration numeric NOT NULL,
+        artist_id varchar,
+        PRIMARY KEY(song_id),
+        CONSTRAINT fk_artist
+            FOREIGN KEY (artist_id)
+                REFERENCES artists (artist_id)
+                ON UPDATE CASCADE ON DELETE CASCADE
+    );
     """
 )
 
 artist_table_create = (
     """
-    CREATE TABLE artists (
-        artist_id VARCHAR(255) PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        location VARCHAR(255),
-        latitude NUMERIC,
-        longitude NUMERIC
-    )    
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id varchar,
+        name varchar NOT NULL,
+        location varchar,
+        latitude numeric,
+        longitude numeric,
+        PRIMARY KEY(artist_id)
+    );    
     """
 )
 
 time_table_create = (
     """
-    CREATE TABLE time (
-        start_time timestamp PRIMARY KEY,
-        hour INTEGER NOT NULL,
-        day INTEGER NOT NULL,
-        week INTEGER NOT NULL,
-        month INTEGER NOT NULL,
-        year INTEGER NOT NULL,
-        weekday INTEGER NOT NULL
-    )    
+    CREATE TABLE IF NOT EXISTS time (
+        start_time timestamp,
+        hour int NOT NULL,
+        day int NOT NULL,
+        week int NOT NULL,
+        month int NOT NULL,
+        year int NOT NULL,
+        weekday int NOT NULL,
+        PRIMARY KEY(start_time)
+    );    
     """
 )
 
@@ -105,5 +118,5 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [ time_table_create, user_table_create, artist_table_create, song_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
